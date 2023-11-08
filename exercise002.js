053d2d2dc06c9f12c255bb0e329baa9e0c112bac
Beanalini <dat826@gmail.com>
2023-11-08 20:22:35 +0000

    feat: add api end point call with async/await

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,21 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
+
+const fetchData = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchData(jsonTypicode);
